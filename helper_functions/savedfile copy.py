bdefa3162afc0c893e0db2d7a9030e87c5cecb9f
neeryks <akashdeep.singh49@gmail.com>
2023-07-08 13:53:22 +0530

    Reverrting Back

---
+++ helper_functions/savedfile copy.py
@@ -0,0 +1,12 @@
+def openai_key():
+    return "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"
+def pexels_api():
+    return "ZWUIqJQX1RkuH9Mu4lP63OFdZWhIcNBliSLRHBsLnhz7rMkCyy71dpks"
+def graph_api():
+    return "EAAHrZBZBZCZBZB0ZAj1ZCZCZBZBZBZDZD"
+def discord_api():
+    return "MTEyMTkwMzExNDY0NDA5OTEzMg.GqlNdJ.f7XAtPuI5_pb5zn6ZeEJfgv9y39UmRwmN35N4U"
+def speech_key():
+    return "8edeb8af513e4b758107cde5cc9b7ee6"
+def caption_key():
+    return ["8edeb8af513e4b758107cde5cc9b7ee6","centralindia"]
